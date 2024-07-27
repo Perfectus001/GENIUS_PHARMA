c780419f4e98f7929bd6adcaa3d15fb300a9896b
Perfectus001 <jeanmikely001@gmail.com>
2024-07-26 22:20:29 -0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Perfectus
+ * Date: 26/07/2024
+ * Time: 22:13
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Genius_Pharmacie
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
